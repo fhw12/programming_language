aaf39ef8cede2da647470cddfef4b2d9786e2849
fhw12 <nikita.pymitron@gmail.com>
2023-01-02 12:19:50 +0700

    AST generation

--- ast.py
+++ ast.py
@@ -15,5 +15,60 @@ class Ast():
 		self.tokens = tokens
 		self.pos = 0
 
+	def get_current_token(self):
+		return self.tokens[self.pos]
+
+	def validation(self, token_type):
+		token = self.get_current_token()
+		if token.type != token_type:
+			print(f"ERROR type ( {token_type} ) => {token}")
+		self.pos += 1
+
+	def NUMBER(self):
+		token = self.get_current_token()
+		if token.type == 'NUMBER':
+			self.validation('NUMBER')
+			node = Number(value=token.value)
+		if token.type == 'LPARENT':
+			self.validation('LPARENT')
+			node = self.PLUS_MINUS()
+		return node
+
+	def MUL_DIV(self):
+		node = self.NUMBER()
+		while self.pos < len(self.tokens):
+			token = self.get_current_token()
+
+			if token.type == 'MUL':
+				self.validation('MUL')
+				node = BinOp(value="*", left=node, right=self.NUMBER())
+			elif token.type == 'DIV':
+				self.validation('DIV')
+				node = BinOp(value="/", left=node, right=self.NUMBER())
+			else:
+				return node
+		return node
+
+	def PLUS_MINUS(self):
+		node = self.MUL_DIV()
+		while self.pos < len(self.tokens):
+			token = self.get_current_token()
+
+			if token.type == 'PLUS':
+				self.validation('PLUS')
+				node = BinOp(value="+", left=node, right=self.MUL_DIV())
+			elif token.type == 'MINUS':
+				self.validation('MINUS')
+				node = BinOp(value="-", left=node, right=self.MUL_DIV())
+			else:
+				return node
+		return node
+
+	def AND(self):
+		pass
+
+	def OR(self):
+		pass
+
 	def parse(self):
 		pass
--- main.py
+++ main.py
@@ -3,12 +3,20 @@ import ast
 
 source = """
 
-(2 + 2) && 2
+2 + 2 * 2
 
 """
 
 tokens = lexer.Lexer(source).parse()
-node = ast.Ast(tokens).parse()
+root_node = ast.Ast(tokens).PLUS_MINUS()
 
-for token in tokens:
-	print(token)
+def print_node(node, i, text):
+	node_type = node.type
+	if node_type == 'BinOp':
+		print(i * '\t' + f'{text}: {node.value}')
+		print_node(node.left, i+1, 'left')
+		print_node(node.right, i+1, 'right')
+	elif node_type == 'Number':
+		print(i * '\t' + f'{text}: {node.value}')
+
+print_node(root_node, 0, 'root')
